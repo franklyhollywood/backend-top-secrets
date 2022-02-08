54aba19a9885726637f3a99a870747ba48d372bd
Frank Hollywood <github@srvsync.com>
2022-02-08 07:57:56 -0800

    started test 2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,27 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345',
+};
+
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+
+// Create an "agent" that gives us the ability
+// to store cookies between requests in a test
+// const agent = request.agent(app);
+
+// Create a user to sign in with
+// const user = await UserService.create({ ...mockUser, ...userProps });
+
+// ...then sign in
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +31,15 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a new user with hashed password', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,20 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users, secrets CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT NOT NULL,
-    password_hash TEXT NOT NULL,
-    
-)
-
-DROP TABLE IF EXISTS secrets CASCADE;
+    password_hash TEXT NOT NULL  
+);
 
 CREATE TABLE secrets (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title BIGINT NOT NULL,
+    title TEXT NOT NULL,
     description TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-)
+);
 

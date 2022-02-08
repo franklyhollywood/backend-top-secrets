f0626ac9c8142d44cabd22336a25ff3d57129cd5
Frank Hollywood <github@srvsync.com>
2022-02-08 10:19:02 -0800

    finished test two and three allow user to login, and get the current user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,27 +2,28 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'test@example.com',
   password: '12345',
 };
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
 
-// Create an "agent" that gives us the ability
-// to store cookies between requests in a test
-// const agent = request.agent(app);
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
 
-// Create a user to sign in with
-// const user = await UserService.create({ ...mockUser, ...userProps });
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
 
-// ...then sign in
-//   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password });
-//   return [agent, user];
-// };
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/session').send({ email, password });
+  return [agent, user];
+};
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -40,6 +41,25 @@ describe('backend routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
+    });
+  });
+
+  it('allows a user to login', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app).post('/api/v1/users/session').send(mockUser);
+    expect(res.body).toEqual({
+      message: 'Signed in successfully!',
+    });
+  });
+
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
 });
